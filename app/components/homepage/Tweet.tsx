19d12d12d70c5921d384081fa77ed5866edaf630
sktbrd <sktbrd.eth@gmail.com>
2024-08-20 01:47:28 -0300

    dom purify

--- app/components/homepage/Tweet.tsx
+++ app/components/homepage/Tweet.tsx
@@ -1,11 +1,14 @@
-// components/homepage/Tweet.tsx
 import { Box, Text, HStack, Button, Avatar } from '@chakra-ui/react';
 import { Comment } from '@hiveio/dhive';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import DOMPurify from 'dompurify';
 
 const Tweet = ({ comment }: { comment: Comment }) => {
+    // Sanitize the comment body to remove any invalid HTML tags or attributes
+    const sanitizedBody = DOMPurify.sanitize(comment.body);
+
     return (
         <Box bg="muted" p={4} mt={1} mb={1} borderRadius="md">
             <HStack mb={2}>
@@ -18,7 +21,7 @@ const Tweet = ({ comment }: { comment: Comment }) => {
                 remarkPlugins={[remarkGfm]}
                 rehypePlugins={[rehypeRaw]}
             >
-                {comment.body}
+                {sanitizedBody}
             </ReactMarkdown>
             <HStack justify="space-between" mt={3}>
                 <Button variant="ghost">Like</Button>
