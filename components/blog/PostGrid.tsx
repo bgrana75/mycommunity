908e3dbacde4940b0ecc48cbb2a8f3d00dca8bd7
bgrana75 <bgrana@gmail.com>
2024-09-17 16:58:03 -0700

    teste

--- components/blog/PostGrid.tsx
+++ components/blog/PostGrid.tsx
@@ -1,11 +1,7 @@
-// components/PostGrid.tsx
 import { SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
 import PostCard from './PostCard';
 import { Discussion } from '@hiveio/dhive';
-//import { Post } from '@/app/types/Posts';
-// I have created a type for the posts in the app/types/Posts.ts file. but may be we can get from dhive or keychain sdk
-
 
 interface PostGridProps {
     posts: Discussion[];
